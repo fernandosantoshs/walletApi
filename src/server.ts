12e7e8a690117456b9031e377cf7ad499373006d
Fernando Santos <fernandosantoshs@gmail.com>
2024-07-08 18:11:50 -0300

    Created Get and Post routes; added session_id migration; changed DATABASE_URL check

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,34 @@
 import fastify from 'fastify';
+import crypto from 'node:crypto';
 import { knex } from './database';
 
 const app = fastify();
 
 app.get('/', async () => {
-  const tables = await knex('sqlite_schema').select('*');
+  const transactions = await knex('transactions').select('*');
 
-  return tables;
+  return transactions;
+});
+
+interface TransactionRequestBody {
+  title: string;
+  amount: number;
+}
+
+app.post('/transaction', async (request, reply) => {
+  const { title, amount } = request.body as TransactionRequestBody;
+
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title,
+      amount,
+    })
+    .returning('*');
+
+  console.log(JSON.stringify(transaction));
+
+  return reply.send(transaction);
 });
 
 app.listen({ port: 3333 }).then(() => console.log('Server is running!'));
