3d5212dc77c85ab8d5f45cda346f8f96a362463d
Fernando Santos <fernandosantoshs@gmail.com>
2024-07-12 17:56:57 -0300

    added zod to validade body/params; added prefix to route transactions; WorkInProgress

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,46 +1,62 @@
 import crypto from 'node:crypto';
+import { z } from 'zod';
 import { knex } from '../database';
 import { FastifyInstance } from 'fastify';
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const createTransactionSchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['debit', 'credit']),
+    });
+
+    const { title, amount, type } = createTransactionSchema.parse(request.body);
+
+    const transaction = await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return reply.status(201).send(transaction);
+  });
+
   app.get('/', async () => {
     const transactions = await knex('transactions').select('*');
 
     return transactions;
   });
 
-  interface TransactionRequestBody {
-    title: string;
-    amount: number;
-  }
+  app.get('/:transactionId', async (request, reply) => {
+    const getTransactionSchema = z.object({
+      transactionId: z.string(),
+    });
 
-  app.post('/transaction', async (request, reply) => {
-    const { title, amount } = request.body as TransactionRequestBody;
+    const { transactionId } = getTransactionSchema.parse(request.params);
 
     const transaction = await knex('transactions')
-      .insert({
-        id: crypto.randomUUID(),
-        title,
-        amount,
-      })
-      .returning('*');
+      .where('id', transactionId)
+      .select('*');
 
-    //console.log(JSON.stringify(transaction));
-
-    return reply.send(transaction);
+    return reply.status(200).send(transaction);
   });
 
-  interface TransactionRequestParams {
-    transactionId: string;
-  }
+  app.put('/:transactionId', async (request, reply) => {
+    const putTransactionSchema = z.object({
+      transactionId: z.string(),
+      title: z.string().nullable(),
+      amount: z.number().nullable(),
+    });
 
-  app.put('/transaction/:transactionId', async (request, reply) => {
-    const { transactionId } = request.params as TransactionRequestParams;
+    const { transactionId } = putTransactionSchema.parse(request.params);
 
-    const { title, amount } = request.body as TransactionRequestBody;
+    const { title, amount } = putTransactionSchema.parse(request.body);
 
     if (!title && !amount)
-      return reply.code(400).send('Error: Both values are null');
+      return reply
+        .code(400)
+        .send('Error: At least one value must be fullfiled');
 
     const updateTransaction = await knex('transactions')
       .where('id', transactionId)
@@ -49,13 +65,17 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return reply.send(updateTransaction);
   });
 
-  app.delete('/transaction/:transactionId', async (request, reply) => {
-    const { transactionId } = request.params as TransactionRequestParams;
+  app.delete('/:transactionId', async (request, reply) => {
+    const deleteTransactionSchema = z.object({
+      transactionId: z.string(),
+    });
+
+    const { transactionId } = deleteTransactionSchema.parse(request.params);
 
     const transaction = await knex('transactions')
       .where('id', transactionId)
       .del();
 
-    return reply.send(transaction);
+    return reply.status(200).send(transaction);
   });
 }
