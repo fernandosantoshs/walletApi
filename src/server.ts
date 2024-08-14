540feddc14b88fc6a57f5a1bbe31645362b333bb
Fernando Santos <fernandosantoshs@gmail.com>
2024-08-14 11:03:02 -0300

    Feat: added post test, started get test

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,4 @@
-import fastify, { FastifyRequest } from 'fastify';
-import cookie from '@fastify/cookie';
+import { app } from './app';
 import { env } from './env';
-import { transactionsRoutes } from './routes/transactions';
-
-const app = fastify();
-
-app.register(cookie);
-app.register(transactionsRoutes, {
-  prefix: 'transactions',
-});
 
 app.listen({ port: env.PORT }).then(() => console.log('Server is running!'));
