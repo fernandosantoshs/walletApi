f0a34531937090869a39730e95736c94a9e08cfd
Fernando Santos <fernandosantoshs@gmail.com>
2024-08-21 10:39:23 -0300

    Added migration script to tests; added test specific transaction

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { it, expect, beforeAll, afterAll, describe } from 'vitest';
+import { it, expect, beforeAll, afterAll, describe, beforeEach } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
+import { execSync } from 'child_process';
 
 describe('Transactions Routes', () => {
   beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transactions Routes', () => {
 
   afterAll(async () => {
     await app.close();
+  });
+
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all');
+    execSync('npm run knex migrate:latest');
   });
 
   it('should be able to create a new transaction', async () => {
@@ -42,5 +48,34 @@ describe('Transactions Routes', () => {
     expect(listTransactionReponse.body).toEqual([
       expect.objectContaining({ title: 'Coxinha', amount: -30 }),
     ]);
+  });
+
+  it('should be able to list a specific transaction', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'Coxinha',
+        amount: 30,
+        type: 'debit',
+      })
+      .expect(201);
+
+    const cookies = createTransactionResponse.get('Set-Cookie') ?? [];
+
+    const listTransactionsReponse = await request(app.server)
+      .get('/transactions/')
+      .set('Cookie', cookies)
+      .expect(200);
+
+    const transactionId = listTransactionsReponse.body[0].id;
+
+    const listSpecificTransactionReponse = await request(app.server)
+      .get(`/transactions/${transactionId}`)
+      .set('Cookie', cookies)
+      .expect(200);
+
+    expect(listSpecificTransactionReponse.body.transaction).toEqual(
+      expect.objectContaining({ title: 'Coxinha', amount: -30 })
+    );
   });
 });
